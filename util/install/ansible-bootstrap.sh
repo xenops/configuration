b735595d940d464d591338a0744bed8f3efd15c2
Nick Gray <nick@crystaldelta.com>
2017-10-30 01:23:49 +1100

    Configuration repo is passed through to edx_ansible (#3)

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -151,7 +151,7 @@ if [[ "true" == "${RUN_ANSIBLE}" ]]; then
     make requirements
 
     cd "${CONFIGURATION_DIR}"/playbooks/edx-east
-    "${PYTHON_BIN}"/ansible-playbook edx_ansible.yml -i '127.0.0.1,' -c local -e "configuration_version=${CONFIGURATION_VERSION}"
+    "${PYTHON_BIN}"/ansible-playbook edx_ansible.yml -i '127.0.0.1,' -c local -e "configuration_version=${CONFIGURATION_VERSION} edx_ansible_source_repo=${CONFIGURATION_REPO}"
 
     # cleanup
     rm -rf "${ANSIBLE_DIR}"
@@ -173,4 +173,3 @@ else
     mkdir -p /edx/ansible/facts.d
     echo '{ "ansible_bootstrap_run": true }' > /edx/ansible/facts.d/ansible_bootstrap.json
 fi
-
